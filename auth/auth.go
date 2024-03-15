23a0de2733609d635233164292112ed7eeb9876a
Muhammad Ridho Fiqri <ridof79@hotmail.com>
2024-03-15 13:01:31 +0700

    refactor jwt auth

--- auth/auth.go
+++ auth/auth.go
@@ -1,12 +1,16 @@
 package auth
 
 import (
+	"net/http"
 	"os"
 	"shopifyx/domain"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	echojwt "github.com/labstack/echo-jwt/v4"
+	"github.com/labstack/echo/v4"
 )
 
 type JwtCustomClaims struct {
@@ -41,4 +45,22 @@ func GenerateAccessToken(user *domain.User) (string, error) {
 	}
 
 	return t, nil
+}
+
+func ConfigJWT() echojwt.Config {
+	return echojwt.Config{
+		SigningKey: []byte(os.Getenv("JWT_SECRET")),
+		Skipper: func(c echo.Context) bool {
+			return strings.HasPrefix(c.Path(), "/v1/user/") || strings.HasPrefix(c.Path(), "/metrics")
+		},
+		NewClaimsFunc: func(c echo.Context) jwt.Claims {
+			return new(JwtCustomClaims)
+		},
+		ErrorHandler: func(c echo.Context, err error) error {
+			if err == echojwt.ErrJWTMissing {
+				return echo.NewHTTPError(http.StatusForbidden, "you dont have access")
+			}
+			return echo.NewHTTPError(http.StatusUnauthorized, "unauthorized1")
+		},
+	}
 }
